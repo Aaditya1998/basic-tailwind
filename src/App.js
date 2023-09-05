c827e8ef49e805358f6cf09829b49de433f7fcad
Aaditya1998 <ninaweaditya@gmail.com>
2023-09-05 16:34:35 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="container mx-auto mt-5">
-      <h1 className='text-2x1 font-semibold text-center'>Welcome to my Tailwind CSS project with React!</h1>
-      <p className='text-gray-600 text-center'>Start building amazing interfaces!</p>
+      <h1 className="text-2x1 font-semibold text-center">
+        Welcome to my Tailwind CSS project with React!
+      </h1>
+      <p className="text-gray-600 text-center">
+        Start building amazing interfaces!
+      </p>
+      <button class="custom-button">Click Me</button>
     </div>
   );
 }
